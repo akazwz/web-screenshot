ad7029710b7b0db578af6f2e97cccb855c9cd375
akazwz <akazwz@pm.me>
2022-02-16 23:10:45 +0800

    dev and prod

--- pages/api/web-screenshot.ts
+++ pages/api/web-screenshot.ts
@@ -1,9 +1,26 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import puppeteer from 'puppeteer'
+import chromium from 'chrome-aws-lambda'
 
 export default async function handler (req: NextApiRequest, res: NextApiResponse) {
-  const browser = await puppeteer.launch({defaultViewport: {width: 1280, height: 800}})
+  /* dev and prod */
+  const browser = process.env.NODE_ENV === 'development'
+    ? await puppeteer.launch({ defaultViewport: { width: 1280, height: 800 } })
+    : await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: {
+        ...chromium.defaultViewport,
+        width: 1280,
+        height: 800,
+      },
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+      env: {
+        ...process.env,
+      }
+    })
+
   const page = await browser.newPage()
   await page.goto('https://fhub-admin.vercel.app/')
   const base64 = await page.screenshot({
@@ -12,4 +29,5 @@ export default async function handler (req: NextApiRequest, res: NextApiResponse
   })
   await browser.close()
   res.status(200).json({ base64: base64 })
+  return
 }
