62d344a12a4fe82bf5f065ff4f2c759414eeb340
akazwz <akazwz@pm.me>
2022-02-17 18:49:24 +0800

    update addExtra

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -2,5 +2,4 @@ import { NextRequest } from 'next/server'
 
 export default function middleware (req: NextRequest) {
   const { geo } = req
-  console.log(geo?.country)
 }
--- pages/api/web-screenshot.ts
+++ pages/api/web-screenshot.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import chromium from 'chrome-aws-lambda'
-import { addExtra, VanillaPuppeteer } from 'puppeteer-extra'
+import { addExtra } from 'puppeteer-extra'
 
 const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 
@@ -16,7 +16,7 @@ export default async function handler (req: NextApiRequest, res: NextApiResponse
   let puppeteer
   let browser
   if (process.env.NODE_ENV === 'production') {
-    puppeteer = addExtra(<VanillaPuppeteer><unknown>chromium.puppeteer)
+    puppeteer = addExtra(chromium.puppeteer)
     puppeteer.use(StealthPlugin())
     browser = await puppeteer.launch({ executablePath: await chromium.executablePath })
   } else {
