1d253081a6881f70d74445a06ca0bf177d985232
akazwz <akazwz@pm.me>
2022-02-26 14:06:12 +0800

    add import

--- pages/api/web-screenshot.ts
+++ pages/api/web-screenshot.ts
@@ -4,22 +4,6 @@ import { addExtra } from 'puppeteer-extra'
 
 // Add the Imports before StealthPlugin
 import 'puppeteer-extra-plugin-stealth/evasions/chrome.app'
-import 'puppeteer-extra-plugin-stealth/evasions/chrome.csi'
-import 'puppeteer-extra-plugin-stealth/evasions/chrome.loadTimes'
-import 'puppeteer-extra-plugin-stealth/evasions/chrome.runtime'
-import 'puppeteer-extra-plugin-stealth/evasions/defaultArgs' // pkg warned me this one was missing
-import 'puppeteer-extra-plugin-stealth/evasions/iframe.contentWindow'
-import 'puppeteer-extra-plugin-stealth/evasions/media.codecs'
-import 'puppeteer-extra-plugin-stealth/evasions/navigator.hardwareConcurrency'
-import 'puppeteer-extra-plugin-stealth/evasions/navigator.languages'
-import 'puppeteer-extra-plugin-stealth/evasions/navigator.permissions'
-import 'puppeteer-extra-plugin-stealth/evasions/navigator.plugins'
-import 'puppeteer-extra-plugin-stealth/evasions/navigator.vendor'
-import 'puppeteer-extra-plugin-stealth/evasions/navigator.webdriver'
-import 'puppeteer-extra-plugin-stealth/evasions/sourceurl'
-import 'puppeteer-extra-plugin-stealth/evasions/user-agent-override'
-import 'puppeteer-extra-plugin-stealth/evasions/webgl.vendor'
-import 'puppeteer-extra-plugin-stealth/evasions/window.outerdimensions'
 
 const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 
