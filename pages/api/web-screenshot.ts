e10d587d523a46388fb7fc7dc29b428c1c135191
akazwz <akazwz@pm.me>
2022-02-26 13:56:38 +0800

    add imports

--- pages/api/web-screenshot.ts
+++ pages/api/web-screenshot.ts
@@ -3,23 +3,23 @@ import chromium from 'chrome-aws-lambda'
 import { addExtra } from 'puppeteer-extra'
 
 // Add the Imports before StealthPlugin
-require('puppeteer-extra-plugin-stealth/evasions/chrome.app')
-require('puppeteer-extra-plugin-stealth/evasions/chrome.csi')
-require('puppeteer-extra-plugin-stealth/evasions/chrome.loadTimes')
-require('puppeteer-extra-plugin-stealth/evasions/chrome.runtime')
-require('puppeteer-extra-plugin-stealth/evasions/defaultArgs') // pkg warned me this one was missing
-require('puppeteer-extra-plugin-stealth/evasions/iframe.contentWindow')
-require('puppeteer-extra-plugin-stealth/evasions/media.codecs')
-require('puppeteer-extra-plugin-stealth/evasions/navigator.hardwareConcurrency')
-require('puppeteer-extra-plugin-stealth/evasions/navigator.languages')
-require('puppeteer-extra-plugin-stealth/evasions/navigator.permissions')
-require('puppeteer-extra-plugin-stealth/evasions/navigator.plugins')
-require('puppeteer-extra-plugin-stealth/evasions/navigator.vendor')
-require('puppeteer-extra-plugin-stealth/evasions/navigator.webdriver')
-require('puppeteer-extra-plugin-stealth/evasions/sourceurl')
-require('puppeteer-extra-plugin-stealth/evasions/user-agent-override')
-require('puppeteer-extra-plugin-stealth/evasions/webgl.vendor')
-require('puppeteer-extra-plugin-stealth/evasions/window.outerdimensions')
+import 'puppeteer-extra-plugin-stealth/evasions/chrome.app'
+import 'puppeteer-extra-plugin-stealth/evasions/chrome.csi'
+import 'puppeteer-extra-plugin-stealth/evasions/chrome.loadTimes'
+import 'puppeteer-extra-plugin-stealth/evasions/chrome.runtime'
+import 'puppeteer-extra-plugin-stealth/evasions/defaultArgs' // pkg warned me this one was missing
+import 'puppeteer-extra-plugin-stealth/evasions/iframe.contentWindow'
+import 'puppeteer-extra-plugin-stealth/evasions/media.codecs'
+import 'puppeteer-extra-plugin-stealth/evasions/navigator.hardwareConcurrency'
+import 'puppeteer-extra-plugin-stealth/evasions/navigator.languages'
+import 'puppeteer-extra-plugin-stealth/evasions/navigator.permissions'
+import 'puppeteer-extra-plugin-stealth/evasions/navigator.plugins'
+import 'puppeteer-extra-plugin-stealth/evasions/navigator.vendor'
+import 'puppeteer-extra-plugin-stealth/evasions/navigator.webdriver'
+import 'puppeteer-extra-plugin-stealth/evasions/sourceurl'
+import 'puppeteer-extra-plugin-stealth/evasions/user-agent-override'
+import 'puppeteer-extra-plugin-stealth/evasions/webgl.vendor'
+import 'puppeteer-extra-plugin-stealth/evasions/window.outerdimensions'
 
 const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 
@@ -35,7 +35,7 @@ export default async function handler (req: NextApiRequest, res: NextApiResponse
 
   let puppeteer
   let browser
-  if (process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV === 'production') {
     puppeteer = addExtra(chromium.puppeteer)
     puppeteer.use(StealthPlugin())
     browser = await puppeteer.launch({
