d7ad1476aa312d36fd7cdd22ed1586d9dba4f6e8
akazwz <akazwz@pm.me>
2022-03-01 13:18:30 +0800

    update

--- pages/api/web-screenshot.ts
+++ pages/api/web-screenshot.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import chromium from 'chrome-aws-lambda'
+import puppeteer from 'puppeteer-core'
 
 export default async function handler (req: NextApiRequest, res: NextApiResponse) {
   const { url } = req.query
@@ -10,16 +11,16 @@ export default async function handler (req: NextApiRequest, res: NextApiResponse
   let browser
   const defaultViewport = { width: 1280, height: 800 }
   if (process.env.NODE_ENV === 'production') {
-    /*browser = await puppeteer.launch({
-      executablePath: await chromium.executablePath,
-      defaultViewport,
-      args: ['--no-sandbox']
-    })*/
-    browser = await chromium.puppeteer.launch({
+    browser = await puppeteer.launch({
       executablePath: await chromium.executablePath,
       defaultViewport,
       args: ['--no-sandbox']
     })
+    /* browser = await chromium.puppeteer.launch({
+       executablePath: await chromium.executablePath,
+       defaultViewport,
+       args: ['--no-sandbox']
+     })*/
   } else {
     const p = require('puppeteer')
     browser = await p.launch({ defaultViewport })
